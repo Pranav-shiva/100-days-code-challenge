d6fe7c1e1587ef7dad6005c6f4fed56ed95bff92
Pranav-shiva <114517193+Pranav-shiva@users.noreply.github.com>
2022-10-06 00:04:23 +0700

    string is palindrome or not

---
+++ Javacodes/String/palindrome string.java
@@ -0,0 +1,29 @@
+/////// to check weather the given string is palindrome or not
+package myprojects;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		String str = "", rev = "";
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a string:");
+		str = sc.nextLine();
+		if (str.equals("")) {
+			System.out.println("no input");
+		} else {
+
+			int length = str.length();
+
+			for (int i = length - 1; i >= 0; i--)
+				rev = rev + str.charAt(i);
+
+			if (str.equals(rev))
+				System.out.println(str + " is a palindrome");
+			else
+				System.out.println(str + " is not a palindrome");
+
+		}
+	}
+}
